823d65b8fe3a9efb4943aaf6c4539bd9a02f84f1
taiwo17 <shoboadefowope2007@gmail.com>
2023-06-24 18:11:41 +0100

    Online DB Added

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
   try {
-    const database = await mongoose.connect('mongodb://0.0.0.0:27017/blog')
+    const database = await mongoose.connect(process.env.DB_ONLINE)
   } catch (error) {
     console.log(error.stack)
   }

300afac0b483a13b9cc1b694e960e580ddc76388
taiwo17 <shoboadefowope2007@gmail.com>
2023-06-25 11:23:01 +0100

    production_url_added

--- docs/server.js
+++ docs/server.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: [
     {
-      url: 'http://localhost:4000/api/v1',
+      url: 'https://blogapibackend.up.railway.app/',
       description: 'Local Server',
     },
   ],

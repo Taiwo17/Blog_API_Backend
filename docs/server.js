484bc581e7d9cd3c9b2d137f11d29a65d23e2d0b
taiwo17 <shoboadefowope2007@gmail.com>
2023-06-25 21:43:13 +0100

    Production Url added

--- docs/server.js
+++ docs/server.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: [
     {
-      url: 'https://blogapibackend-production.up.railway.app/',
+      url: 'https://blogapibackend-production.up.railway.app/api/v1',
       description: 'Live Server',
     },
   ],
